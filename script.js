bebc4f582e7567db4feee69f4c496d5bb84ebf8a
RandyB2024 <Buijsr@icloud.com>
2026-06-08 13:43:25 +0200

    Refine discovery sample placement

--- script.js
+++ script.js
@@ -173,7 +173,7 @@
         ${reference}
         <p class="scent-group">${scentGroup}</p>
         <p class="product-short">${product.omschrijving}</p>
-        <p class="sample-mini">Gratis ORIV&egrave;A Discovery Sample bij iedere bestelling.</p>
+        <p class="sample-mini">Gratis ORIV&Eacute;A Discovery Sample bij iedere bestelling.</p>
         <div class="product-purchase">
           ${variantSelector}
           ${premiumInfo}
